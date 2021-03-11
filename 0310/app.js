bf0c0a34ae35cb848d887ceefd091cc253c5622e
jaiboy825 <jaiboy@naver.com>
2021-03-11 15:49:53 +0900

    0311 과제 끝

--- 0310/app.js
+++ 0310/app.js
@@ -1,13 +1,18 @@
 window.onload = function () {
     $(".pop").hide();
-    $(".sub_menu").hide();
-    $(".menu").hover(function () {
-        $(".sub_menu").slideDown();
-    }, function () {
-        $(".sub_menu").slideUp();
+    $(".sub_menu > li").hide();
+    let sub_menus = $(".sub_menu > li");
+
+    $(".main_menu > li").mouseenter(function () {
+        sub_menus.stop().slideUp();
+        let index = $(this).index();
+        sub_menus.eq(index).stop().slideDown();
+    });
+    $(".menu").mouseleave(function () {
+        sub_menus.stop().slideUp();
     });
     $(".slider > img").css({ "left": "100%" });
-    $(".slider > img").eq(0).css({"left" : 0});
+    $(".slider > img").eq(0).css({ "left": 0 });
     let ment = ["복지는 인권", "우리 모두 함께", "사람이 먼저다"];
     let idx = 0;
     setInterval(() => {
@@ -18,9 +23,9 @@ window.onload = function () {
         idx = next;
     }, 3000);
 }
-function openPop(){
+function openPop() {
     $(".pop").show();
 }
-function closePop(){
+function closePop() {
     $(".pop").hide();
 }
